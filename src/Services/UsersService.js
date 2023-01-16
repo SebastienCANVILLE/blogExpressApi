e661a4d701c3865e60449e3ebb06e54c45d8ef1a
MARMUS Laure (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7805361n@commun.ad.sncf.fr>
2023-01-16 20:58:55 +0100

    modif

--- src/Services/UsersService.js
+++ src/Services/UsersService.js
@@ -5,7 +5,7 @@ class UsersService {
 
     async addUser(username,hash) {
 
-        const data = await client.query('INSERT INTO users (username,password) VALUES ($1,$2) RETURNING *', [username,hash]);
+        const data = await client.query('INSERT INTO users (username,password) VALUES ($1,$2) RETURNING * ', [username,hash]);
 
         if (data.rowCount) {
 
